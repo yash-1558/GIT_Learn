ab1170ecb2f9f8effaa99427d3f62b363165fdf6
Yash <107048847+yash-1558@users.noreply.github.com>
2026-05-17 21:01:02 +0530

    feat: add field c to Test1, fix gitignore

--- src/Test1.java
+++ src/Test1.java
@@ -1,10 +1,12 @@
 public class Test1 {
     private final int a;
     private final String b;
+    private final int c;
 
-    public Test1(int a, String b) {
+    public Test1(int a, String b, int c) {
         this.a = a;
         this.b = b;
+        this.c = c;
     }
 
 }
